1ead80bf99a3b3e9436f6c60b8ea4cd7a9f2189f
Korzre <manueldanilo931@gmail.com>
2019-07-17 02:58:12 +0200

    first commit

---
+++ Jogo em Teste/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Jogo_em_Teste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Goldenrod;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
+            this.button1.FlatAppearance.BorderSize = 3;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(26, 22);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(169, 140);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.DarkRed;
+            this.button2.FlatAppearance.BorderSize = 3;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(426, 22);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 140);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Crimson;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.MidnightBlue;
+            this.button3.FlatAppearance.BorderSize = 3;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(237, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(169, 140);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.SeaGreen;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.Color.Yellow;
+            this.button4.FlatAppearance.BorderSize = 3;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.Location = new System.Drawing.Point(581, 289);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(169, 140);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Fenice BT", 250F);
+            this.textBox1.Location = new System.Drawing.Point(26, 68);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(680, 401);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "0";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 130;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(42, 416);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(723, 485);
+            this.panel1.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("JudasCaps Wd", 71.99999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(33, 203);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(671, 198);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tem cuidado com\r\n o tempo!";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.HotTrack;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SunkenVertical;
+            this.dataGridView1.ClipboardCopyMode = System.Windows.Forms.DataGridViewClipboardCopyMode.EnableAlwaysIncludeHeaderText;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("!Sketchy Times", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.DimGray;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.GridColor = System.Drawing.Color.Yellow;
+            this.dataGridView1.Location = new System.Drawing.Point(2, 503);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.dataGridView1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(244, 46);
+            this.dataGridView1.TabIndex = 9;
+            // 
+            // Column1
+            // 
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.DodgerBlue;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("!Basket of Hammers", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.DimGray;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.FillWeight = 200F;
+            this.Column1.HeaderText = "Pontuações";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "";
+            this.Column2.Name = "Column2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(788, 550);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.IsMdiContainer = true;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+    }
+}
+
---
+++ Jogo em Teste/Form1.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Jogo_em_Teste
+{
+    public partial class Form1 : Form
+    {
+        private OleDbConnection conexao;
+        private OleDbCommand comando;
+        private string strcon = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=pontuacoes.mdb";
+        
+        public Form1()
+        {
+            InitializeComponent();
+            dataGridView1.Visible = false;
+            progressBar1.Visible = false;
+            panel1.Visible = false;
+            textBox1.Visible = false;
+            label2.Text = "BE CAREFULL WITH\nTIME!";
+            Carrega();
+        }
+
+        public void Carrega()
+        {
+           
+        }
+
+        public void Gravar()
+        {
+           
+        }
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Increment(1);
+
+            if (progressBar1.Value == 1)
+            {
+                button1.Visible = false;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+            }
+            if (progressBar1.Value == 30)
+            {
+                button1.Visible = true;
+                button2.Visible = false;
+                button3.Visible = false;
+                button4.Visible = false;
+            }
+            if (progressBar1.Value == 33)
+            {
+                button4.Visible = true;
+                button2.Visible = false;
+                button3.Visible = false;
+                button1.Visible = false;
+            }
+            if (progressBar1.Value == 35)
+            {
+                button2.Visible = true;
+                button4.Visible = false;
+                button3.Visible = false;
+                button1.Visible = false;
+            }
+            if (progressBar1.Value == 39)
+            {
+                button1.Visible = true;
+                button4.Visible = false;
+                button3.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 42)
+            {
+                button3.Visible = true;
+                button4.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 46)
+            {
+                button4.Visible = true;
+                button3.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 49)
+            {
+                button1.Visible = true;
+                button4.Visible = false;
+                button3.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 52)
+            {
+                button3.Visible = true;
+                button4.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 56)
+            {
+                button2.Visible = true;
+                button4.Visible = false;
+                button1.Visible = false;
+                button3.Visible = false;
+            }
+            if (progressBar1.Value == 62)
+            {
+                button3.Visible = true;
+                button4.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 65)
+            {
+                button4.Visible = true;
+                button4.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 69)
+            {
+                button1.Visible = true;
+                button4.Visible = false;
+                button3.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 72)
+            {
+                button3.Visible = true;
+                button4.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 77)
+            {
+                button1.Visible = true;
+                button4.Visible = false;
+                button3.Visible = false;
+                button2.Visible = false;
+            }
+            if (progressBar1.Value == 81)
+            {
+                button3.Visible = false;
+                button4.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+                panel1.Visible = true;
+            }
+            if (progressBar1.Value == 92)
+            {
+                button3.Visible = false;
+                button4.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+                panel1.Visible = false;
+                textBox1.Visible = true;
+                textBox1.Location = new Point(52, 76);
+                textBox1.Size = new Size(680, 401);
+                if (txtbox == 0)
+                {
+                    Application.Exit();
+                }
+                else
+                {
+                    Gravar();
+                }
+                
+            }
+            if (progressBar1.Value == 100)
+            {
+                button3.Visible = false;
+                button4.Visible = false;
+                button1.Visible = false;
+                button2.Visible = false;
+                panel1.Visible = false;
+
+                Form1 vic = new Form1();
+                vic.Show();
+                this.Hide();
+                timer1.Stop();
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int txtbox;
+            txtbox = Convert.ToInt32(textBox1.Text);
+
+
+            txtbox++;
+
+            textBox1.Text = (txtbox).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            txtbox++;
+
+            textBox1.Text = (txtbox).ToString();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+          
+
+            txtbox++;
+
+            textBox1.Text = (txtbox).ToString();
+
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            txtbox++;
+
+            textBox1.Text = (txtbox).ToString();
+        }
+
+        public int txtbox { get; set; }
+    }
+}
